6e2b68b709053328b440b5ccb19a1b1fef9e53eb
Julia Camacho <juliac123@gmail.com>
2024-03-06 13:40:03 -0500

    update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,22 @@
+<script>
+    import projects from '$lib/projects.json';
+    import Project from "$lib/Project.svelte";
+</script>
 <svelte:head>
 	<title>Home</title>
 </svelte:head>
 <h1>Julia Camacho</h1>
-    <p>Hi! I'm a junior majoring in Course 11-6 (Urban Science and Planning with Computer Science).</p>
-    <img src="images/sf_headshot.jpg" class="home" alt="A photo of myself standing against a body of water in San Francisco.">
+<p>Hi! I'm a junior majoring in Course 11-6 (Urban Science and Planning with Computer Science).</p>
+<img src="images/sf_headshot.jpg" class="home" alt="A photo of myself standing against a body of water in San Francisco.">
+<h2>My Projects</h2>
+<div class="projects">
+    {#each projects.slice(0, 3) as p}
+        <Project info={p} hLevel=3 />
+    {/each}
+</div>
+
+<style>
+    h2 {
+	    font-size: 300%;
+    }
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,16 +1,13 @@
 <script>
     import projects from '$lib/projects.json';
+    import Project from "$lib/Project.svelte";
 </script>
 <svelte:head>
 	<title>Projects</title>
 </svelte:head>
 <h1>My {projects.length} Projects</h1>
-    <div class="projects">
-        {#each projects as p}
-            <article>
-                <h2>{p.title}</h2>
-                <img src="{p.image}" alt="">
-                <p>{p.description}</p>
-            </article>
-        {/each}
-    </div>
+<div class="projects">
+    {#each projects as p}
+        <Project info={p} />
+    {/each}
+</div>
