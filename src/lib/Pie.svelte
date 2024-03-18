3ebf56ef0926a52d3e159c207cc760ab358cd4ee
Julia Camacho <juliac123@gmail.com>
2024-03-18 15:09:43 -0400

    step 2

--- src/lib/Pie.svelte
+++ src/lib/Pie.svelte
@@ -3,27 +3,73 @@
 
     let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-    let data = [1, 2, 3, 4, 5, 5];
-    let sliceGenerator = d3.pie();
+    let data = [
+        { value: 1, label: "apples" },
+        { value: 2, label: "oranges" },
+        { value: 3, label: "mangos" },
+        { value: 4, label: "pears" },
+        { value: 5, label: "limes" },
+        { value: 5, label: "cherries" }
+    ];
+
+    let sliceGenerator = d3.pie().value(d => d.value);
     let arcData = sliceGenerator(data);
     let arcs = arcData.map(d => arcGenerator(d));
 
     let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 </script>
+<div class="container">
+	<svg viewBox="-50 -50 100 100">
+        {#each arcs as arc, i}
+            <path d={ arc } fill={ colors(i) } />
+        {/each}
+    </svg>
+    <ul class="legend">
+        {#each data as d, index}
+            <li style="--color: { colors(index) }">
+                <span class="swatch"></span>
+                {d.label} <em>({d.value})</em>
+            </li>
+        {/each}
+    </ul>
+</div>     
 
-<svg viewBox="-50 -50 100 100">
-	{#each arcs as arc, i}
-        <path d={ arc } fill={ colors(i) } />
-    {/each}
-</svg>
 
 <style>
+    div.container {
+        display: flex;
+        gap: 5em;
+    }
+
     svg {
         max-width: 20em;
         margin-block: 2em;
 
         /* Do not clip shapes outside the viewBox */
         overflow: visible;
+    }
+
+    li {
+        display: flex;
+        align-items: center;
+    }
+
+    ul.legend {
+        flex: 1;
+        list-style-type: none;
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(9em, 1fr));
+        border-style: solid;
+        border-color: #c9c9c9;
+        padding: 1em;
+    }
+
+    span.swatch {
+        display: inline-block;
+        width: 1em;
+        height: 1em;
+        background-color: var(--color);
+        margin-right: 1em;
     }
 </style>
